ced2844bcec35872bac1edc7d6d626e2866b4ee9
LinseNain <141765188+LinseNain@users.noreply.github.com>
2023-11-24 20:00:24 +0000

    Finish

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}  </h1>
-          <h2>${variables.role} </h2>
-          <h3>${variables.city} ${variables.country} </h3>
+          <h1>${variables.name ? variables.name : "Name"}${
+    variables.lastName ? variables.lastname : " Last Name"
+  }  </h1>
+          <h2>${variables.role ? variables.role : "Role"} </h2>
+          <h3>${variables.city ? variables.city : "City"} ${
+    variables.country ? variables.country : "Country"
+  } </h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter} "><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github} "><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${variables.linkedin} "><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram} "><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            } "><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            } "><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            } "><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            } "><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -64,11 +76,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: "Name",
-    lastName: "Last Name",
-    role: "Role",
-    country: "Country",
-    city: "City"
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
   };
   render(window.variables); // render the card for the first time
 
